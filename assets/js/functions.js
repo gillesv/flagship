8ddc196c11e0a4ef3f5da10d7496a2d294769323
Gilles Vandenoostende <gilles.vandenoostende@duvalguillaume.com>
2015-05-28 10:42:21 +0200

    working taalswitch

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,9 +2,52 @@ $(document).ready(function() {
 	// dead simple paging
 	var $app = $("#app"),
 		activepageclass = "active-page-",
-		activepage = "info";
+		activepage = "info",
+		overlayerOpen = false;
 	
-	$('nav a').click(function(evt) {
+	// check URL hash
+	if(window.location.hash.toString() === '') {
+		$app.toggleClass(activepageclass + activepage, true);
+	} else {
+		var hash = window.location.hash.toString().split("#").join('').toString();
+		
+		// valid hash? 
+		switch(hash) {
+			case "info":
+			case "bio":
+			case "about":
+				activepage = hash;
+			break;
+			case "about-history":
+				activepage = "about";
+				overlayerOpen = true;
+			break;
+			default: 
+				activepage = "info";
+			break;
+		}
+		
+		$app.toggleClass(activepageclass + activepage, true);
+		$('#about').toggleClass('showOverlayer', overlayerOpen);
+	}
+	
+	// language nav
+	$('nav.langswitch a').click(function(evt) {
+		evt.preventDefault();
+		
+		var href = $(this).attr("href").toString();
+		
+		if(overlayerOpen) {
+			href = href + "#" + activepage + "-history";
+		}else {
+			href = href + "#" + activepage;
+		}
+		
+		window.location = href;		
+	});
+	
+	// main nav
+	$('nav#topnav a').click(function(evt) {
 		evt.preventDefault();
 		
 		var href = $(this).attr("href").toString().split("#").join("").toString();
@@ -17,16 +60,21 @@ $(document).ready(function() {
 		activepage = href;		
 	});
 	
+	// open/close overlayer	
 	$('#about .button.info').click(function(evt) {
 		evt.preventDefault();
 		
-		$('#about').toggleClass('showOverlayer', true);
+		overlayerOpen = true;
+		
+		$('#about').toggleClass('showOverlayer', overlayerOpen);
 	});
 	
 	$('#history .button.back').click(function(evt) {
 		evt.preventDefault();
 		
-		$('#about').toggleClass('showOverlayer', false);
+		overlayerOpen = false;
+		
+		$('#about').toggleClass('showOverlayer', overlayerOpen);
 	});
 });
 
